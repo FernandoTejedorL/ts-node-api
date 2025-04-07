0b55bb84ced157ffbff05afe75e19447086dc5d4
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2025-04-07 15:54:56 +0200

    update

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import usersRoute from './users';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+app.use(express.json());
+app.use('/', usersRoute);
 
 app.listen(3000, () => {
   console.log('listening on port 3000...');
---
+++ users/index.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+const router = Router();
+
+type User = {
+  id: string;
+  name: string;
+};
+
+router.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+router.post('/', (req, res) => {
+  const { name } = req.body as User;
+  console.log(name);
+  res.send('ok');
+});
+
+export default router;
